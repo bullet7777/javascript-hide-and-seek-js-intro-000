c11ba3cab3cb7ed891923092e990eaebceb3c606
Chris <chris.mahfouz@hotmail.com>
2021-03-11 10:10:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget(){
   return document.getElementById('nested').querySelector('div.target')
 }
 function increaseRankBy(n){
-  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < upRank.length; i++){
     upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
